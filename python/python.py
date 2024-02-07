9e6e61ad56d23ba3f8c7011dcd2f8e5330df8976
abdulrahmanai01 <abdulai01@brightskills.com>
2024-02-07 13:10:23 +0530

    added files

--- python/python.py
+++ python/python.py
@@ -11,7 +11,24 @@ elif "cube" in b:
 else:
     print("Please choose the correct option.")
 '''
+'''
 a=input("enter the value you want")
 a=int(a)
 for b in range (1,11):
   print(a,"x",b,"=",a*b)
+'''
+'''
+n=int(input("enter the number."))
+if(n%2)==0:
+    print("the given number is Even.")
+else:
+    print ("the given number is odd")
+'''
+n=float(input("Enter the number."))
+if n>0:
+    print("You enterded positive number.")
+elif n==0:
+    print("you enterded zero.")
+else:
+    print("you enterded negative number.")
+ 
